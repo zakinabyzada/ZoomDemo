cb35e284a9434202dfa752159afa3c6541997717
zaki <zaki@localhost>
2023-05-23 17:31:32 -0700

    WebDriver  code

--- src/test/java/zoom1/zoomDemo.java
+++ src/test/java/zoom1/zoomDemo.java
@@ -1,8 +1,17 @@
 package zoom1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class zoomDemo {
 	public static void main(String[] args) {
-		System.out.println("Example");
-	}
 		
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.letskodeit.com/practice");
+	}
+	
 }
