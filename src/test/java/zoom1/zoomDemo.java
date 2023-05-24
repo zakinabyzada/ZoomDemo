7d0e3df6aad1703a57844beb1514c439c4bcaea8
zaki <zaki@localhost>
2023-05-23 17:57:41 -0700

    BmW Radio

--- src/test/java/zoom1/zoomDemo.java
+++ src/test/java/zoom1/zoomDemo.java
@@ -1,17 +1,26 @@
 package zoom1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class zoomDemo {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InterruptedException {
+
 		WebDriverManager.chromedriver().setup();
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.letskodeit.com/practice");
+		Thread.sleep(2000);
+
+		WebElement radioBMW = driver.findElement(By.id("bmwRadio"));
+		radioBMW.click();
+
+		Thread.sleep(3000);
+		driver.quit();
 	}
 	
 }
